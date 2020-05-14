4675e928a9549b32276d88f151759859ffe1e05e
Marshall Raiskin <marshall.raiskin@gmail.com>
2020-05-14 00:12:10 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -112,8 +112,8 @@ def changing_alan
         :languages => ["C"]
       }
     }
-    programmer_hash[:alan_kay][:known_for] => "GUI"
-    programmer_hash
+    programmer_hash[:alan_kay][:known_for] = "GUI"
+     programmer_hash
 
 end
 
